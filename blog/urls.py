1a48259d81a3e72b74e4422f5e54cc39d2a2875b
tologonkubandykov <marmokk@icloud.com>
2024-10-28 13:52:53 +0600

    htmls

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.base, name='base'),
+    path('index/', views.index, name='index'),
+    path('history/', views.history, name='history'),
+    path('about/', views.about, name='about'),
+    path('faculties/', views.faculties, name='faculties'),
+    path('admissions/', views.admissions, name='admissions'),
+    path('contact/', views.contact, name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
+def base(request):
+    return render(request, 'blog/base.html')
+
 def index(request):
     return render(request, 'blog/index.html')
+
+def history(request):
+    return render(request, 'blog/history.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+def faculties(request):
+    return render(request, 'blog/faculties.html')
+
+def admissions(request):
+    return render(request, 'blog/admissions.html')
+
+def contact(request):
+    return render(request, 'blog/contact.html')
